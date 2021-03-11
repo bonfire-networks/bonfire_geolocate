3c7da76f9a800ee5349b7304381ddd5c4714c656
Mayel <git@mayel.space>
2021-03-11 16:57:44 +0100

    change admin

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.Geolocate.Integration do
   def is_admin(user) do
-    if Map.get(user, :local_user) do
-      Map.get(user.local_user, :is_instance_admin)
+    if Map.get(user, :instance_admin) do
+      Map.get(user.instance_admin, :is_instance_admin)
     else
       false # FIXME
     end
