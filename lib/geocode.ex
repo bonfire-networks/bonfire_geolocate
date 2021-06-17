4225db0c16cef668b3a66d428304d3ecd1ba38aa
Mayel <git@mayel.space>
2021-06-17 18:43:19 +0200

    maybe

--- lib/geocode.ex
+++ lib/geocode.ex
@@ -2,14 +2,20 @@ defmodule Bonfire.Geolocate.Geocode do
 
   def coordinates(query) do
     # use the default provider (OpenStreetMaps)
-    with {:error, nil} <- Geocoder.call(query),
+    with {:error, _} <- maybe_with_geocoder(query),
     # use an alternative provider. If `key` is not specified here the globally defined key will be used.
-    {:error, nil} <- maybe_call_opencage(query) do
+    {:error, _} <- maybe_with_opencage(query) do
       {:error, nil}
     end
   end
 
-  def maybe_call_opencage(query) do
+  defp maybe_with_geocoder(query) do
+    Geocoder.call(query)
+  catch _ ->
+    {:error, nil}
+  end
+
+  defp maybe_with_opencage(query) do
     key = System.get_env("GEOLOCATE_OPENCAGEDATA")
     if key && key !="" do
       Geocoder.call(query, provider: Geocoder.Providers.OpenCageData, key: key)
