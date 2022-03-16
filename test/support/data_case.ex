73077b6542eb0d130c3ad6a6fd9d7f975fd3b744
Mayel de Borniol <git@mayel.space>
2022-03-17 12:36:27 +1300

    Clean tests

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -30,11 +30,7 @@ defmodule Bonfire.Geolocate.DataCase do
 
     import Bonfire.Common.Config, only: [repo: 0]
 
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(repo())
-
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(repo(), {:shared, self()})
-    end
+    Bonfire.Common.Test.Interactive.setup_test_repo(tags)
 
     :ok
   end
