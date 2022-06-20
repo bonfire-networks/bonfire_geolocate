db92adf013b613aa102cda12c3fb9daf2247c8f7
Mayel de Borniol <git@mayel.space>
2022-06-20 14:28:01 +1200

    runtime

---
+++ lib/runtime_config.ex
@@ -0,0 +1,12 @@
+defmodule Bonfire.Geolocate.RuntimeConfig do
+
+  def config_module, do: true
+
+  def config do
+    import Config
+
+    config :bonfire, :js_config,
+      mapbox_api_key: System.get_env("MAPBOX_API_KEY")
+
+  end
+end
