552b66d40ec95197f802cdad568e997fe6801e6f
Mayel de Borniol <git@mayel.space>
2024-02-06 18:53:46 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/243

--- priv/repo/migrations/20200523081012_init_pointers.exs
+++ priv/repo/migrations/20200523081012_init_pointers.exs
@@ -1,4 +1,4 @@
-defmodule Bonfire.Geolocate.Repo.Migrations.InitPointers  do
+defmodule Bonfire.Repo.Migrations.InitPointers do
   @moduledoc false
   use Ecto.Migration
   import Needle.Migration
--- priv/repo/migrations/20201208094940_import_geolocation.exs
+++ priv/repo/migrations/20201208094940_import_geolocation.exs
@@ -1,4 +1,4 @@
-defmodule Bonfire.Repo.Migrations.ImportGeolocation  do
+defmodule Bonfire.Repo.Migrations.ImportGeolocation do
   @moduledoc false
   use Ecto.Migration
 
