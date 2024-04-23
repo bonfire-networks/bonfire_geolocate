ecd2b9e4dd68da08086fdaa0e7c9770f4dc0b04a
Mayel de Borniol <git@mayel.space>
2024-04-23 23:41:40 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/915

--- lib/hydration.ex
+++ lib/hydration.ex
@@ -1,47 +1,45 @@
-if Code.ensure_loaded?(Bonfire.API.GraphQL) do
-  defmodule Bonfire.Geolocate.GraphQL.Hydration do
-    alias Bonfire.API.GraphQL.CommonResolver
+defmodule Bonfire.Geolocate.GraphQL.Hydration do
+  alias Bonfire.API.GraphQL.CommonResolver
 
-    def hydrate() do
-      %{
-        spatial_thing: %{
-          # FIXME:
-          # canonical_url: [
-          #   resolve: &CommonsPub.Characters.GraphQL.Resolver.canonical_url_edge/3
-          # ],
-          # display_username: [
-          #   resolve: &CommonsPub.Characters.GraphQL.Resolver.display_username_edge/3
-          # ],
-          in_scope_of: [
-            resolve: &CommonResolver.context_edge/3
-          ]
-        },
-        geolocation_query: %{
-          spatial_thing: [
-            resolve: &Bonfire.Geolocate.GraphQL.geolocation/2
-          ],
-          spatial_things_pages: [
-            resolve: &Bonfire.Geolocate.GraphQL.geolocations/2
-          ],
-          spatial_things: [
-            resolve: &Bonfire.Geolocate.GraphQL.all_geolocations/2
-          ]
-        },
-        geolocation_mutation: %{
-          create_spatial_thing: [
-            resolve: &Bonfire.Geolocate.GraphQL.create_geolocation/2
-          ],
-          update_spatial_thing: [
-            resolve: &Bonfire.Geolocate.GraphQL.update_geolocation/2
-          ],
-          delete_spatial_thing: [
-            resolve: &Bonfire.Geolocate.GraphQL.delete_geolocation/2
-          ]
-        }
-        # geo_scope: [
-        #   resolve_type: &CommonResolver.resolve_context_type/2
-        # ]
+  def hydrate() do
+    %{
+      spatial_thing: %{
+        # FIXME:
+        # canonical_url: [
+        #   resolve: &CommonsPub.Characters.GraphQL.Resolver.canonical_url_edge/3
+        # ],
+        # display_username: [
+        #   resolve: &CommonsPub.Characters.GraphQL.Resolver.display_username_edge/3
+        # ],
+        in_scope_of: [
+          resolve: &CommonResolver.context_edge/3
+        ]
+      },
+      geolocation_query: %{
+        spatial_thing: [
+          resolve: &Bonfire.Geolocate.GraphQL.geolocation/2
+        ],
+        spatial_things_pages: [
+          resolve: &Bonfire.Geolocate.GraphQL.geolocations/2
+        ],
+        spatial_things: [
+          resolve: &Bonfire.Geolocate.GraphQL.all_geolocations/2
+        ]
+      },
+      geolocation_mutation: %{
+        create_spatial_thing: [
+          resolve: &Bonfire.Geolocate.GraphQL.create_geolocation/2
+        ],
+        update_spatial_thing: [
+          resolve: &Bonfire.Geolocate.GraphQL.update_geolocation/2
+        ],
+        delete_spatial_thing: [
+          resolve: &Bonfire.Geolocate.GraphQL.delete_geolocation/2
+        ]
       }
-    end
+      # geo_scope: [
+      #   resolve_type: &CommonResolver.resolve_context_type/2
+      # ]
+    }
   end
 end
