047ccb4a0e4b6d35e4773e3339e30a923175ec94
Mayel de Borniol <git@mayel.space>
2025-07-06 08:32:30 +0100

    weather

--- lib/runtime_config.ex
+++ lib/runtime_config.ex
@@ -14,5 +14,21 @@ defmodule Bonfire.Geolocate.RuntimeConfig do
       object_preview: [
         {Bonfire.Geolocate.Geolocation, Bonfire.Geolocate.PlacePreviewLive}
       ]
+
+    if api_key = System.get_env("OPEN_WEATHER_MAP_API_KEY") do
+      config :forecastr,
+        backend: Forecastr.OWM,
+        appid: api_key,
+        # minutes to cache
+        ttl: 14 * 60_000
+    end
+
+    if api_key = System.get_env("PIRATE_WEATHER_API_KEY") do
+      config :forecastr,
+        backend: Forecastr.PirateWeather,
+        appid: api_key,
+        # minutes to cache
+        ttl: 14 * 60_000
+    end
   end
 end
