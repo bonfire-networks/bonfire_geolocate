fe76adcbfbdd306e966b993d15465d464566b1ca
Mayel de Borniol <git@mayel.space>
2025-08-27 11:05:08 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1518

--- lib/web/components/widgets/widget_forecast_live.ex
+++ lib/web/components/widgets/widget_forecast_live.ex
@@ -2,4 +2,15 @@ defmodule Bonfire.Geolocate.WidgetForecastLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop location, :string, default: nil
+
+  declare_settings(:select, l("Measurement Units"),
+    keys: [:measurement_units],
+    options: [
+      metric: l("Metric (meters, celsius, etc)"),
+      imperial: l("Imperial (miles, fahrenheit, etc)")
+    ],
+    default_value: :metric,
+    description: l("Select units to use for distance, temperature, etc."),
+    scope: :user
+  )
 end
